264945bcd1bdae2df54fb08f31a092d7eb293df9
HemaAmruthaDasari <137883961+HemaAmruthaDasari@users.noreply.github.com>
2023-08-04 22:08:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Design from './components/Design';
+function App() {
+  return (
+    <div className="App">
+      <Design/>
+    </div>
+  );
+}
+
+export default App;
